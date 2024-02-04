28d014c891997dbbb72870bcc020099d2667bacd
ay-chiba1647 <153065961+ay-chiba1647@users.noreply.github.com>
2024-02-04 17:54:01 +0900

    kadai_011を追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+ <head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編課題11</title>
+ </head>
+
+ <body>
+  <p>
+    <?php
+    $onion = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+    foreach ($onion as $key => $value) {
+      echo "{$key}:{$value}<br>";
+    }
+    ?>
+  </p>
+ </body>
+</html>
